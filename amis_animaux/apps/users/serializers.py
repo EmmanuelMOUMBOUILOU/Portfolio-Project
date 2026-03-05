13fdf6bb144da61522c347af0eddb1a61f2729de
EmmanuelMOUMBOUILOU <emmanuelmoumbouilou@gmail.com>
2026-03-05 23:04:54 +0100

    feat(users): add serializers

---
+++ amis_animaux/apps/users/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Profile
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password")
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(source="user.id", read_only=True)
+    username = serializers.CharField(source="user.username", read_only=True)
+    email = serializers.EmailField(source="user.email", read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = ("user_id", "username", "email", "bio", "avatar", "location", "created_at", "updated_at")
+        read_only_fields = ("created_at", "updated_at")
